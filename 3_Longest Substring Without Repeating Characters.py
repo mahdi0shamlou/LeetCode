d78d1b3e8523e5717b28b89acab5bfedc1876b48
mahdi0shamlou <shamlouprogramer@gmail.com>
2024-12-25 14:17:59 -0500

    Solve LeetCode q3 in o(n)

--- 3_Longest Substring Without Repeating Characters.py
+++ 3_Longest Substring Without Repeating Characters.py
@@ -1,5 +1,20 @@
 class Solution(object):
     def lengthOfLongestSubstring(self, s):
+        list_numbers = {}
+        max_len = 0
+        for i in range(0,len(s)):
+            if s[i] in list_numbers:
+                if max_len < len(list_numbers):
+                    max_len = len(list_numbers)
+                list_numbers = {}
+                list_numbers[s[i]] = 1
+            else:
+                list_numbers[s[i]] = 1
+
+        return max_len
+
+
+    def lengthOfLongestSubstring_v2(self, s):
         list_numbers = []
         max_len = 0
         for i in range(0,len(s)):
@@ -12,6 +27,8 @@ class Solution(object):
 
         return max_len
 
+
+
 x = Solution()
-r = x.lengthOfLongestSubstring("abcabcbb")
+r = x.lengthOfLongestSubstring("aoloopsol")
 print(r)
