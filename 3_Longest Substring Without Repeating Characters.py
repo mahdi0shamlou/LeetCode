9f2bfc4901119b5f04d488ff9aa9f6ce72e4c3ba
mahdi0shamlou <shamlouprogramer@gmail.com>
2024-12-27 09:14:32 -0500

    Update and complete qustions 3

--- 3_Longest Substring Without Repeating Characters.py
+++ 3_Longest Substring Without Repeating Characters.py
@@ -1,34 +1,33 @@
 class Solution(object):
+
     def lengthOfLongestSubstring(self, s):
-        list_numbers = {}
         max_len = 0
-        for i in range(0,len(s)):
-            if s[i] in list_numbers:
-                if max_len < len(list_numbers):
-                    max_len = len(list_numbers)
-                list_numbers = {}
-                list_numbers[s[i]] = 1
-            else:
-                list_numbers[s[i]] = 1
+        lists = []
+        for i in range(0, len(s)):
+            for z in range(i, len(s)):
+                if s[z] in lists:
+                    lists = []
+                    break
+                else:
+                    lists.append(s[z])
 
-        return max_len
-
-
-    def lengthOfLongestSubstring_v2(self, s):
-        list_numbers = []
-        max_len = 0
-        for i in range(0,len(s)):
-            if s[i] in list_numbers:
-                if max_len < len(list_numbers):
-                    max_len = len(list_numbers)
-                list_numbers = [s[i]]
-            else:
-                list_numbers.append(s[i])
+                if max_len < len(lists):
+                    max_len = len(lists)
 
         return max_len
 
 
 
 x = Solution()
-r = x.lengthOfLongestSubstring("aoloopsol")
+r = x.lengthOfLongestSubstring("abbcds")
+print(r)
+r = x.lengthOfLongestSubstring("abcabc")
+print(r)
+r = x.lengthOfLongestSubstring(" ")
+print(r)
+r = x.lengthOfLongestSubstring("")
+print(r)
+r = x.lengthOfLongestSubstring("aabbccc")
+print(r)
+r = x.lengthOfLongestSubstring("dvdf")
 print(r)
