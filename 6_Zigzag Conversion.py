27072a0a727bfff5e97bc1d9c600b8802b31faa0
mahdi shamlou <shamlouprogrammer@gmail.com>
2026-01-07 13:56:31 +0000

    refactor(6_Zigzag Conversion): solve the problem

---
+++ 6_Zigzag Conversion.py
@@ -0,0 +1,45 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+
+        result = []
+        for i in range(len(s)):
+            result.append("")
+
+        numRows_counter = 0
+        numRows_counter_way = 1
+
+        for i in s:
+            try:
+
+                result[numRows_counter] = str(result[numRows_counter]) + str(i)
+
+                if numRows_counter + 1 == numRows:
+                    numRows_counter_way = -1
+                if numRows_counter - 1 < 0:
+                    numRows_counter_way = +1
+
+                if numRows_counter_way == 1:
+                    numRows_counter = numRows_counter + 1
+                else:
+                    numRows_counter = numRows_counter - 1
+            except:
+                pass
+        final_res = ""
+        for i in result:
+            final_res = final_res + i
+        print(result)
+        return final_res
+
+
+
+x = Solution()
+r = x.convert(s = "PAYPALISHIRING", numRows = 4)
+print("%" * 100)
+print(r)
+print('PINALSIGYAHRPI')
+
+x = Solution()
+r = x.convert(s = "AB", numRows = 2)
+print("%" * 100)
+print(r)
+print('AB')
