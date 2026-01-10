3793abfa1919dcbe25c3b7b554d48e31d9627d67
mahdi shamlou <shamlouprogrammer@gmail.com>
2026-01-10 08:30:27 +0000

    refactor(3_Longest Substring Without Repeating Characters): create a new version for with more performance and better io for this question

--- 3_Longest Substring Without Repeating Characters.py
+++ 3_Longest Substring Without Repeating Characters.py
@@ -1,6 +1,9 @@
+from measure_execution_time import timing_decorator
+
 class Solution(object):
 
-    def lengthOfLongestSubstring(self, s):
+    @timing_decorator
+    def lengthOfLongestSubstring_old_version(self, s):
         max_len = 0
         lists = []
         for i in range(0, len(s)):
@@ -15,19 +18,28 @@ class Solution(object):
                     max_len = len(lists)
 
         return max_len
+    
 
+    @timing_decorator
+    def lengthOfLongestSubstring(self, s):
+        last_seen = {}
+        left = 0
+        max_len = 0
+        
+        for right, char in enumerate(s):
+            if char in last_seen and last_seen[char] >= left:
+                left = last_seen[char] + 1
+                
+            last_seen[char] = right
+            max_len = max(max_len, right - left + 1)
+            
+        return max_len
 
 
 x = Solution()
-r = x.lengthOfLongestSubstring("abbcds")
+
+r = x.lengthOfLongestSubstring_old_version("dvdfaksndasd"*11000)
 print(r)
-r = x.lengthOfLongestSubstring("abcabc")
-print(r)
-r = x.lengthOfLongestSubstring(" ")
-print(r)
-r = x.lengthOfLongestSubstring("")
-print(r)
-r = x.lengthOfLongestSubstring("aabbccc")
-print(r)
-r = x.lengthOfLongestSubstring("dvdf")
+
+r = x.lengthOfLongestSubstring("dvdfaksndasd"*11000)
 print(r)
