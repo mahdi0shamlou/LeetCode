feca953b60df5e0f109910b1cf92e6b654f66004
mahdi0shamlou <shamlouprogramer@gmail.com>
2026-01-23 09:30:36 -0500

    refactor(7_Reverse Integer): solve this problem but i have an error input is and signed number not and str

--- 7_Reverse Integer.py
+++ 7_Reverse Integer.py
@@ -1,6 +1,9 @@
 class Solution:
+
     def reverse(self, x: int) -> int:
-        
+        print("*"*30)
+        print(f"x for start : {x}")
+
         reversed_x = 0
         sign = +1
 
@@ -9,13 +12,26 @@ class Solution:
             sign = -1
 
         print(f"sign is : {sign}")
-        
-        for i in range(len(x)):
-            pass
+        print(f"x befor revese x : {x}")
+
+        for i in range(len(x), 0, -1):
+            if reversed_x == 0:
+                reversed_x = int(x[i-1])
+            else:
+                reversed_x = (reversed_x*10) + int(x[i-1])
+
+            if (reversed_x * sign) > ((2**31)-1) or (reversed_x * sign) < ((2**31) * -1):
+                return 0
         
         return reversed_x * sign
 
 
 x = Solution()
 r = x.reverse(x = "-123")
+print(r)
+r = x.reverse(x = "-123000")
+print(r)
+r = x.reverse(x = "-10023")
+print(r)
+r = x.reverse(x = "120")
 print(r)
