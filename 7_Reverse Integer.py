0c8f6b2bf92f1ee68e53ce86dd1c81ff569dc324
mahdi0shamlou <shamlouprogramer@gmail.com>
2026-01-23 09:09:59 -0500

    refactor(7_Reverse Integer): update revers method how to detect sign and try to reverse

--- 7_Reverse Integer.py
+++ 7_Reverse Integer.py
@@ -1,3 +1,21 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        pass
+        
+        reversed_x = 0
+        sign = +1
+
+        if "-" == x[0]:
+            x = x[1:]
+            sign = -1
+
+        print(f"sign is : {sign}")
+        
+        for i in range(len(x)):
+            pass
+        
+        return reversed_x * sign
+
+
+x = Solution()
+r = x.reverse(x = "-123")
+print(r)
